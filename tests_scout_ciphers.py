8ec585ce526d4e52c20fe3a22b76468cb28c5a16
konrad <konradmartynowski1@gmail.com>
2019-01-04 21:14:31 +0100

    added unittest for cesar function in scout-ciphers

---
+++ tests_scout_ciphers.py
@@ -0,0 +1,19 @@
+import unittest
+import scout_ciphers
+
+
+class TestScout(unittest.TestCase):
+    def test_cipher(self):
+        self.assertAlmostEqual(scout_ciphers.cesar('aBcD', 1), 'BCDE')
+        self.assertAlmostEqual(scout_ciphers.cesar('aBcD', 5), 'FGHI')
+        self.assertAlmostEqual(scout_ciphers.cesar('aBcD'), 'NOPQ')
+        self.assertRaises(ValueError, scout_ciphers.cesar, '')
+        self.assertRaises(ValueError, scout_ciphers.cesar, None)
+        self.assertRaises(ValueError, scout_ciphers.cesar, True)
+        self.assertRaises(ValueError, scout_ciphers.cesar, 'ab%cd')
+        self.assertRaises(ValueError, scout_ciphers.cesar, 'abcd', None)
+        self.assertRaises(ValueError, scout_ciphers.cesar, 'abcd', True)
+        self.assertRaises(ValueError, scout_ciphers.cesar, 'abcd', 0)
+        self.assertRaises(ValueError, scout_ciphers.cesar, 'abcd', -1)
+        self.assertRaises(ValueError, scout_ciphers.cesar, 'abcd', 2.5)
+        self.assertRaises(ValueError, scout_ciphers.cesar, 'abcd', -2.5)
